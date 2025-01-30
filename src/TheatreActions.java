5e7e1ffd4e62771d49a64d5c0741f6a6e0375dc2
Dharneesh S <ddharnish42@gmail.com>
2025-01-30 08:16:44 -0800

    Update TheatreActions.java

--- src/TheatreActions.java
+++ src/TheatreActions.java
@@ -3,18 +3,18 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class TheatreActions {//theatre actions class
-    public static HashMap<String, HashSet<Show>> theatreAgainstShow(ArrayList<Movies> movieAvailable) {
-        HashMap<String, HashSet<Show>> theatreAgainstShow = new HashMap<>();
+    public static HashMap<String, HashSet<Show>> theatreShow(ArrayList<Movies> movieAvailable) {
+        HashMap<String, HashSet<Show>> theatreShow = new HashMap<>();
         for (var currentMovie : movieAvailable) {// get all the movie object fields which is entered by the user (only the user location and date)
 
-            if (theatreAgainstShow.containsKey(currentMovie.getTheatre().getTheatrename())) {// if theatre name already exists
-                theatreAgainstShow.get(currentMovie.getTheatre().getTheatrename()).add(currentMovie.getShow());// add the show object
+            if (theatreShow.containsKey(currentMovie.getTheatre().getTheatrename())) {// if theatre name already exists
+                theatreShow.get(currentMovie.getTheatre().getTheatrename()).add(currentMovie.getShow());// add the show object
             } else {
                 HashSet<Show> show = new HashSet<>();// create new hash set
                 show.add(currentMovie.getShow());// add to hash set
-                theatreAgainstShow.put(currentMovie.getTheatre().getTheatrename(), show);// add to hashmap
+                theatreShow.put(currentMovie.getTheatre().getTheatrename(), show);// add to hashmap
             }
         }
-        return theatreAgainstShow;
+        return theatreShow;
     }
 }
